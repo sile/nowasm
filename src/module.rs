76b62a85c1d8658ecc18c71bce2f991fd033547d
Takeru Ohta <phjgt308@gmail.com>
2024-06-13 21:51:10 +0900

    Add Allocator trsit

--- src/module.rs
+++ src/module.rs
@@ -1,11 +1,15 @@
-#[derive(Debug)]
-pub struct Module {}
-
-impl Module {
-    pub fn decode(wasm: &[u8]) -> Result<Module, DecodeError> {
-        Ok(Module {})
-    }
-}
+pub trait Allocator {}
 
 #[derive(Debug)]
 pub enum DecodeError {}
+
+#[derive(Debug)]
+pub struct Module<A> {
+    allocator: A,
+}
+
+impl<A: Allocator> Module<A> {
+    pub fn decode(wasm: &[u8]) -> Result<Self, DecodeError> {
+        todo!()
+    }
+}
