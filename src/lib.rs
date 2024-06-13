c7380bc243d13ffb0ada66684104bea31f1b1d2e
Takeru Ohta <phjgt308@gmail.com>
2024-06-13 09:43:15 +0900

    [WIP] Add test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
 #![no_std]
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_address() -> orfail::Result<()> {
+        //serde_json::from_str(include_str!("../testdata/address.json"));
+        Ok(())
     }
 }
