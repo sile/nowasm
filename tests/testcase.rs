c12958fd02c2ff47058ab8935d5225c136e5b28c
Takeru Ohta <phjgt308@gmail.com>
2024-06-13 20:14:26 +0900

    Add testcase commands

--- tests/testcase.rs
+++ tests/testcase.rs
@@ -1,6 +1,6 @@
 use orfail::OrFail;
 use serde::Deserialize;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Deserialize)]
 pub struct Testcase {
@@ -24,8 +24,48 @@ impl Testcase {
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum Command {
-    Module,
-    AssertReturn,
-    AssertTrap,
-    AssertMalformed,
+    Module {
+        filename: PathBuf,
+    },
+    AssertReturn {
+        action: Action,
+        expected: Vec<Value>,
+    },
+    AssertTrap {
+        action: Action,
+        text: String,
+        expected: Vec<Value>,
+    },
+    AssertMalformed {
+        filename: PathBuf,
+        text: String,
+        module_type: String,
+    },
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Action {
+    Invoke { field: String, args: Vec<Value> },
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Value {
+    I32 {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        value: Option<String>,
+    },
+    I64 {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        value: Option<String>,
+    },
+    F32 {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        value: Option<String>,
+    },
+    F64 {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        value: Option<String>,
+    },
 }
