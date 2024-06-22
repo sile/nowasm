68d977fba3a89ba2ea344a368bd6d1f7b1d132ff
Takeru Ohta <phjgt308@gmail.com>
2024-06-22 14:30:06 +0900

    Add sign-extension instructions

--- src/instructions_sign_extension.rs
+++ src/instructions_sign_extension.rs
@@ -1,12 +1,23 @@
 use crate::{reader::Reader, DecodeError};
 
 #[derive(Debug, Clone, Copy)]
-pub enum SignExtensionInstr {}
+pub enum SignExtensionInstr {
+    I32Extend8S,
+    I32Extend16S,
+    I64Extend8S,
+    I64Extend16S,
+    I64Extend32S,
+}
 
 impl SignExtensionInstr {
     pub fn decode(reader: &mut Reader) -> Result<Self, DecodeError> {
         let opcode = reader.read_u8()?;
         match opcode {
+            0xc0 => Ok(SignExtensionInstr::I32Extend8S),
+            0xc1 => Ok(SignExtensionInstr::I32Extend16S),
+            0xc2 => Ok(SignExtensionInstr::I64Extend8S),
+            0xc3 => Ok(SignExtensionInstr::I64Extend16S),
+            0xc4 => Ok(SignExtensionInstr::I64Extend32S),
             _ => Err(DecodeError::InvalidOpcode { value: opcode }),
         }
     }
