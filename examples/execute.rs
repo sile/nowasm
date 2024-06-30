ee9aba913c708c245ddd17a094623d6088631e6c
Takeru Ohta <phjgt308@gmail.com>
2024-06-30 18:37:29 +0900

    [WIP] Add execute.rs

---
+++ examples/execute.rs
@@ -0,0 +1,161 @@
+use clap::Parser;
+use nowasm::{
+    symbols::{Code, Data, Elem, Export, Global, Import, TableType, ValType},
+    FuncType, Instr, Locals, Module, Vectors,
+};
+use orfail::{Failure, OrFail};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+struct Args {
+    wasm_path: PathBuf,
+    function_name: String,
+    args: Vec<i32>,
+}
+
+pub fn main() -> orfail::Result<()> {
+    let args = Args::parse();
+    let wasm_bytes = std::fs::read(&args.wasm_path).or_fail()?;
+
+    let _module = Module::decode(&wasm_bytes, StdVectors::default())
+        .map_err(|e| Failure::new(format!("{e:?}")))
+        .or_fail()?;
+
+    Ok(())
+}
+
+#[derive(Debug, Default)]
+pub struct StdVectors {
+    pub bytes: Vec<u8>,
+    pub val_types: Vec<ValType>,
+    pub instrs: Vec<Instr>,
+    pub idxs: Vec<u32>,
+    pub locals: Vec<Locals>,
+    pub func_types: Vec<FuncType>,
+    pub imports: Vec<Import>,
+    pub table_types: Vec<TableType>,
+    pub globals: Vec<Global>,
+    pub exports: Vec<Export>,
+    pub elems: Vec<Elem>,
+    pub codes: Vec<Code>,
+    pub datas: Vec<Data>,
+}
+
+impl Vectors for StdVectors {
+    fn bytes(&self) -> &[u8] {
+        &self.bytes
+    }
+
+    fn bytes_append(&mut self, bytes: &[u8]) -> bool {
+        self.bytes.extend_from_slice(bytes);
+        true
+    }
+
+    fn val_types(&self) -> &[ValType] {
+        &self.val_types
+    }
+
+    fn val_types_append(&mut self, items: &[ValType]) -> bool {
+        self.val_types.extend_from_slice(items);
+        true
+    }
+
+    fn instrs(&self) -> &[Instr] {
+        &self.instrs
+    }
+
+    fn instrs_append(&mut self, items: &[Instr]) -> bool {
+        self.instrs.extend_from_slice(items);
+        true
+    }
+
+    fn idxs(&self) -> &[u32] {
+        &self.idxs
+    }
+
+    fn idxs_append<T: Copy + Into<u32>>(&mut self, idxs: &[T]) -> bool {
+        self.idxs.extend(idxs.iter().map(|&x| x.into()));
+        true
+    }
+
+    fn locals(&self) -> &[Locals] {
+        &self.locals
+    }
+
+    fn locals_append(&mut self, items: &[Locals]) -> bool {
+        self.locals.extend_from_slice(items);
+        true
+    }
+
+    fn func_types(&self) -> &[FuncType] {
+        &self.func_types
+    }
+
+    fn func_types_append(&mut self, items: &[FuncType]) -> bool {
+        self.func_types.extend_from_slice(items);
+        true
+    }
+
+    fn imports(&self) -> &[Import] {
+        &self.imports
+    }
+
+    fn imports_append(&mut self, items: &[Import]) -> bool {
+        self.imports.extend_from_slice(items);
+        true
+    }
+
+    fn table_types(&self) -> &[TableType] {
+        &self.table_types
+    }
+
+    fn table_types_append(&mut self, items: &[TableType]) -> bool {
+        self.table_types.extend_from_slice(items);
+        true
+    }
+
+    fn globals(&self) -> &[Global] {
+        &self.globals
+    }
+
+    fn globals_append(&mut self, items: &[Global]) -> bool {
+        self.globals.extend_from_slice(items);
+        true
+    }
+
+    fn exports(&self) -> &[Export] {
+        &self.exports
+    }
+
+    fn exports_append(&mut self, items: &[Export]) -> bool {
+        self.exports.extend_from_slice(items);
+        true
+    }
+
+    fn elems(&self) -> &[Elem] {
+        &self.elems
+    }
+
+    fn elems_append(&mut self, items: &[Elem]) -> bool {
+        self.elems.extend_from_slice(items);
+        true
+    }
+
+    fn codes(&self) -> &[Code] {
+        &self.codes
+    }
+
+    fn codes_append(&mut self, items: &[Code]) -> bool {
+        self.codes.extend_from_slice(items);
+        true
+    }
+
+    fn datas(&self) -> &[Data] {
+        &self.datas
+    }
+
+    fn datas_append(&mut self, items: &[Data]) -> bool {
+        self.datas.extend_from_slice(items);
+        true
+    }
+}
