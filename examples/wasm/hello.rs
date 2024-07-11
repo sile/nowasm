96d1920a40e994876fd7171b52cd3ce0435b01af
Takeru Ohta <phjgt308@gmail.com>
2024-07-11 20:20:38 +0900

    Add hello example

---
+++ examples/wasm/hello.rs
@@ -0,0 +1,12 @@
+// $ cargo build --target wasm32-unknown-unknown --example hello
+extern "C" {
+    fn print(s: *const u8, len: i32);
+}
+
+#[no_mangle]
+pub fn hello_world() {
+    let msg = "Hello, world!\n";
+    unsafe {
+        print(msg.as_ptr(), msg.len() as i32);
+    }
+}
